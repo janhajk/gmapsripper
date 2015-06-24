e4094b855f9006ecd94df76574835457235dd5cc
janhajk <janhajk@gmail.com>
2015-06-24 13:19:39 +0000

    attempt to fix canvas problem

--- ripper.js
+++ ripper.js
@@ -1,16 +1,29 @@
+// As of 2015, google put a copyprotection on their maps-canvas, which
+// ends in always getting a black image
+// SOlution has not yet been found
+
+
 // get gmap-Canvas (this contains the rendered map)
 (function() {
 
-var gCanvas = (document.getElementsByTagName("canvas"))[1],
-bigmapw = 5000,
-bigmaph = 5000,
+var gCanvas = (document.getElementsByTagName("canvas"))[0],
+bigmapw = 1000,
+bigmaph = 1000,
 record  = false,
 
 xx = bigmapw/2-gCanvas.width/2,
 yy = bigmaph/2-gCanvas.height/2,
 x  = 0,
 y  = 0,
-svgtag = '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink">',
+xmlns = 'http://www.w3.org/2000/svg',
+xlinkns = 'http://www.w3.org/1999/xlink',
+
+svgtag = function() {
+    var svgElem = document.createElementNS (xmlns, 'svg');
+    svgElem.setAttributeNS (null, 'width', 30);
+    svgElem.setAttributeNS (null, 'height', 30);
+    return svgElem;
+},
 
 bigmap = document.createElement('canvas'),
 
@@ -66,33 +79,118 @@ divWerbung = function() {
 },
 
 btnOpen = function() {
-  var o = document.createElement('div');
-  o.id = 'btnOpen';
-  o.style.width    = '30px';
-  o.style.height   = '30px';
-  o.style.position = 'absolute';
-  o.style.cursor   = 'pointer';
-  o.style.bottom   = '0px';
-  o.style.right    = '10px';
-  o.title          = 'open map';
-  o.innerHTML      = svgtag+'<defs><linearGradient id="lg" x2="50%" x1="50%" ><stop style="stop-color:#fcf702" offset="0" /><stop style="stop-color:#ee8f00" offset="1" /></linearGradient><linearGradient xlink:href="#lg" y2="100%" id="lg1" /><linearGradient xlink:href="#lg" y1="100%" id="lg2" /></defs><path style="fill:url(#lg2);stroke:#A70;stroke-linejoin:round" d="m 2,1.5 4.5,0 0,-1 7,0 0,1 c 2.5,0 6,0 9,0 0.5,0 1,0.5 1.5,1.5 l 2,15.5 c 0,0.5 -0.5,1.5 -1.5,1.5 l -20.5,0 c -0.5,0 -1,-0.5 -1.5,-1.5 L 0.5,3 C 0.5,2 1,1.5 2,1.5 z" /><path style="fill:url(#lg1);stroke:#A70;stroke-linejoin:round" d="m 7.5,3 7,0 0,1.5 c 4.5,0 9,0 14,0 0.5,0 1.5,0.5 1.5,1.5 l -3,13.5 c 0,0.5 -0.5,1.5 -1.5,1.5 l -20.5,0 c -0.5,0 -1.5,-0.5 -1.5,-1.5 l 3,-14.5 c 0,-0.5 0.5,-1.5 1.5,-1.5 z" /></svg>';
-  o.addEventListener ('click', function(){outputrip();}, false);
-  return o;
+    var o = document.createElement('div');
+    o.id = 'btnOpen';
+    o.style.width    = '30px';
+    o.style.height   = '30px';
+    o.style.position = 'relative';
+    o.style.cursor   = 'pointer';
+    o.title          = 'open map';
+    var svg = svgtag();
+    var defs = document.createElementNS (xmlns, 'defs');
+
+    var grad = document.createElementNS (xmlns, 'linearGradient');
+    grad.setAttributeNS (null, 'id', 'lg');
+    grad.setAttributeNS (null, 'x1', '50%');
+    grad.setAttributeNS (null, 'x2', '50%');
+    var stopTop = document.createElementNS (xmlns, 'stop');
+    stopTop.setAttributeNS (null, 'offset', 0);
+    stopTop.setAttributeNS (null, 'stop-color', '#fcf702');
+    grad.appendChild (stopTop);
+    var stopBottom = document.createElementNS (xmlns, 'stop');
+    stopBottom.setAttributeNS (null, 'offset', 1);
+    stopBottom.setAttributeNS (null, 'stop-color', '#ee8f00');
+    grad.appendChild (stopBottom);
+    defs.appendChild (grad);
+
+    var grad = document.createElementNS (xmlns, 'linearGradient');
+    grad.setAttributeNS(null, 'id', 'lg1');
+    grad.setAttributeNS(null, 'y2', '100%');
+    grad.setAttributeNS(xlinkns, 'xlink:href', '#lg');
+    defs.appendChild(grad);
+
+    var grad = document.createElementNS (xmlns, 'linearGradient');
+    grad.setAttributeNS (null, 'id', 'lg2');
+    grad.setAttributeNS (null, 'y1', '100%');
+    grad.setAttributeNS(xlinkns, 'xlink:href', '#lg');
+    defs.appendChild(grad);
+    svg.appendChild(defs);
+
+    var path = document.createElementNS(xmlns, 'path');
+    path.setAttributeNS (null, 'fill', 'url(#lg2)');
+    path.setAttributeNS (null, 'stroke', '#A70');
+    path.setAttributeNS (null, 'stroke-linejoin', 'round');
+    path.setAttributeNS (null, 'd', 'm 2,1.5 4.5,0 0,-1 7,0 0,1 c 2.5,0 6,0 9,0 0.5,0 1,0.5 1.5,1.5 l 2,15.5 c 0,0.5 -0.5,1.5 -1.5,1.5 l -20.5,0 c -0.5,0 -1,-0.5 -1.5,-1.5 L 0.5,3 C 0.5,2 1,1.5 2,1.5 z');
+    svg.appendChild(path);
+
+    var path = document.createElementNS(xmlns, 'path');
+    path.setAttributeNS (null, 'fill', 'url(#lg1)');
+    path.setAttributeNS (null, 'stroke', '#A70');
+    path.setAttributeNS (null, 'stroke-linejoin', 'round');
+    path.setAttributeNS (null, 'd', 'm 7.5,3 7,0 0,1.5 c 4.5,0 9,0 14,0 0.5,0 1.5,0.5 1.5,1.5 l -3,13.5 c 0,0.5 -0.5,1.5 -1.5,1.5 l -20.5,0 c -0.5,0 -1.5,-0.5 -1.5,-1.5 l 3,-14.5 c 0,-0.5 0.5,-1.5 1.5,-1.5 z');
+    svg.appendChild(path);
+
+    o.appendChild(svg);
+    o.addEventListener ('click', function(){outputrip();}, false);
+    return o;
 },
 
-btnRecord = function() {
-  var o = document.createElement('div');
-  o.id = 'btnRecord';
-  o.style.width    = '30px';
-  o.style.height   = '30px';
-  o.style.position = 'absolute';
-  o.style.cursor   = 'pointer';
-  o.style.bottom   = '10px';
-  o.style.left     = '10px';
-  o.title          = 'start recording';
-  o.innerHTML      = svgtag+'<defs><linearGradient id="lg3" x1="50%" y1="50%" x2="70%" y2="80%"><stop style="stop-color:#f83717" offset="0" /><stop style="stop-color:#fdbaaf" offset="1" /></linearGradient><linearGradient id="lg4" y1="50%" x2="50%" ><stop style="stop-color:#fb6654" offset="0" /><stop style="stop-color:#fd9385" offset="1" /></linearGradient></defs><path style="fill:url(#lg3)" d="m 208,166 a 71,71 0 1 1 -142,0 71,71 0 1 1 142,0 z" transform="matrix(0.209,0,0,0.209,-13.59,-19.68)" /><path style="fill:url(#lg4)" d="M 15,0 C 7,0 0,6.5 0,15 c 0,2.5 0.5,4.5 1.5,6.5 6.5,-7 15.5,-11 26,-11 0.5,0 1,0 1.5,0 C 27.5,4.5 21.5,0 15,0 z" /></svg>';
-  o.addEventListener ('click', function(){record = true;}, false);
-  return o;
+    btnRecord = function() {
+    var o = document.createElement('div');
+    o.id = 'btnRecord';
+    o.style.width    = '30px';
+    o.style.height   = '30px';
+    o.style.position = 'relative';
+    o.style.cursor   = 'pointer';
+    o.style.float    = 'right';
+    o.title          = 'start recording';
+    var svg = svgtag();
+    var defs = document.createElementNS(xmlns, 'defs');
+
+    var grad = document.createElementNS (xmlns, 'linearGradient');
+    grad.setAttributeNS(null, 'id', 'lg3');
+    grad.setAttributeNS(null, 'x1', '50%');
+    grad.setAttributeNS(null, 'x2', '70%');
+    grad.setAttributeNS(null, 'y1', '50%');
+    grad.setAttributeNS(null, 'y2', '80%');
+    var stopTop = document.createElementNS(xmlns, 'stop');
+    stopTop.setAttributeNS(null, 'offset', 0);
+    stopTop.setAttributeNS(null, 'stop-color', '#f83717');
+    grad.appendChild(stopTop);
+    var stopBottom = document.createElementNS(xmlns, 'stop');
+    stopBottom.setAttributeNS(null, 'offset', 1);
+    stopBottom.setAttributeNS(null, 'stop-color', '#fdbaaf');
+    grad.appendChild(stopBottom);
+    defs.appendChild(grad);
+
+    var grad = document.createElementNS(xmlns, 'linearGradient');
+    grad.setAttributeNS(null, 'id', 'lg4');
+    grad.setAttributeNS(null, 'x2', '50%');
+    grad.setAttributeNS(null, 'y1', '50%');
+    var stopTop = document.createElementNS(xmlns, 'stop');
+    stopTop.setAttributeNS(null, 'offset', 0);
+    stopTop.setAttributeNS(null, 'stop-color', '#fb6654');
+    grad.appendChild(stopTop);
+    var stopBottom = document.createElementNS(xmlns, 'stop');
+    stopBottom.setAttributeNS(null, 'offset', 1);
+    stopBottom.setAttributeNS(null, 'stop-color', '#fd9385');
+    grad.appendChild(stopBottom);
+    defs.appendChild(grad);
+    svg.appendChild(defs);
+
+    var path = document.createElementNS(xmlns, 'path');
+    path.setAttributeNS(null, 'fill', 'url(#lg3)');
+    path.setAttributeNS(null, 'd', 'm 208,166 a 71,71 0 1 1 -142,0 71,71 0 1 1 142,0 z');
+    path.setAttributeNS(null, 'transform', 'matrix(0.209,0,0,0.209,-13.59,-19.68)');
+    svg.appendChild(path);
+    var path = document.createElementNS(xmlns, 'path');
+    path.setAttributeNS(null, 'fill', 'url(#lg4)');
+    path.setAttributeNS(null, 'd', 'M 15,0 C 7,0 0,6.5 0,15 c 0,2.5 0.5,4.5 1.5,6.5 6.5,-7 15.5,-11 26,-11 0.5,0 1,0 1.5,0 C 27.5,4.5 21.5,0 15,0 z');
+    svg.appendChild(path);
+    o.appendChild(svg);
+
+    o.addEventListener('click', function(){record = true;}, false);
+    return o;
 },
 
 bmOverview = function() {
@@ -117,16 +215,17 @@ bmOverview.prototype.create = function() {
   bmGroup.height             = this.h;
   bmGroup.style.width        = this.w + 'px';
   bmGroup.style.height       = this.h + 'px';
-  bmGroup.style.bottom       = '200px';
+  bmGroup.style.top          = '160px';
   bmGroup.style.left         = '13px';
   bmGroup.style.position     = 'absolute';
   bmGroup.style.boxShadow    = '0px 1px 4px rgba(0,0,0,0.3)';
   bmGroup.style.borderRadius = '2px';
   bmGroup.style.border       = '2px solid white';
+  bmGroup.style.zIndex       = '10000';
+  bmGroup.appendChild(this.wd);
+  bmGroup.appendChild(btnOpen());
+  bmGroup.appendChild(btnRecord());
   document.getElementsByTagName('body')[0].appendChild(bmGroup);
-  document.getElementById('bmGroup').appendChild(this.wd);
-  document.getElementById('bmGroup').appendChild(btnOpen());
-  document.getElementById('bmGroup').appendChild(btnRecord());
 };
 bmOverview.prototype.update = function(x,y,w,h) {
   x = Math.round(x/bigmapw*this.w);
